a3d5fe74e38460560b1d7ab242bdbf9c06d479a8
JOSEPH CHEGE <josephchege08@gmail.com>
2017-08-21 10:37:04 +0300

    modified java script

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
-var pingPong = function(number) {
-  if ((number % 3 === 0) || (number % 5 === 0)) {
-    return true;
-  } else {
-    return false;
-  };
-};
-
-
 $(document).ready(function() {
   $("form#formInput").submit(function(event) {
     $("ul").empty();
